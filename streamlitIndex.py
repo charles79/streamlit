ffd525ea5ca392a9ec168008b94f6d1cd8b08c1e
charles79 <charles.alc@gmail.com>
2023-10-15 23:58:21 -0500

    Update streamlitIndex.py

--- streamlitIndex.py
+++ streamlitIndex.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from datetime import time
 
+st.text('Texto de Prueba')
+
 appointment = st.slider("Schedule your appointment:", value=(time(11, 30), time(12, 45)))
 st.write("You're scheduled for:", appointment)
